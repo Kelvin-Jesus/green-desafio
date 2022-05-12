5633097ba71fe546619f958e18e1e810bb8bf3bd
Kelvin <kj.theprog@gmail.com>
2022-05-12 16:32:13 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <header-principal></header-principal>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import HeaderPrincipal from './components/HeaderPrincipal.vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        components: {
+            HeaderPrincipal
+        }
+    };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/HeaderPrincipal.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>Usuários</h1>
+</template>
+
+<script>
+
+    export default {
+        name: 'HeaderPrincipal'
+    };
+
+</script>
+
+<style>
+
+</style>
